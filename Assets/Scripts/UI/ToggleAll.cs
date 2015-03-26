03e2373315caf752bd7985fe260b3938de87de39
Marc Teyssier <marc.teys@gmail.com>
2015-03-26 17:05:16 +0100

    adding tons of images

---
+++ Assets/Scripts/UI/ToggleAll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleAll : MonoBehaviour {
+
+    public GameObject[] allObjects;
+
+	
+	public void SetActiveAll (bool setA)
+	{
+        for (int i = 0; i < allObjects.Length; i++)
+        {
+            allObjects[i].SetActive(setA);
+        }
+	}
+
+}
