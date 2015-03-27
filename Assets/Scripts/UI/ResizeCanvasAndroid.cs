35d283ce36d3bca6072b54a4a12291a75ab2a9c9
Marc Teyssier <marc.teys@gmail.com>
2015-03-27 17:51:41 +0100

    adding lot of elements

---
+++ Assets/Scripts/UI/ResizeCanvasAndroid.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResizeCanvasAndroid : MonoBehaviour {
+    public CanvasScaler can;
+
+	void Start ()
+	{
+	#if UNITY_ANDROID
+        if (Application.platform == RuntimePlatform.Android) can.scaleFactor = 1.0f;
+    #endif
+
+	}
+	
+	void Update ()
+	{
+	
+	}
+}
